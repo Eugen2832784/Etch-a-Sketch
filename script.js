17b7012ea7da53980544af0913fad48fde281c5a
EugeneDev <kovalonokjevgenij@gmail.com>
2026-01-12 20:08:03 +0200

    refactor for adaptive square dispaly

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 const okno = document.querySelector("#container");
+let inputNum = pageOpen();
 
-for (let i = 0; i < 256; i++) {
+let size = 16;
+
+if (inputNum >= 1 && inputNum <= 100) {
+  size = inputNum;
+}
+
+okno.innerHTML = "";
+okno.style.width = "500px";
+let squareSize = 500 / size;
+
+for (let i = 0; i < size * size; i++) {
   const square = document.createElement("div");
   square.classList.add("grid_square");
+
+  square.style.width = squareSize + "px";
+  square.style.height = squareSize + "px";
+
   okno.appendChild(square);
-  square.addEventListener("mousemove", function () {
-    square.style.cssText = "background-color: black;";
+
+  square.addEventListener("mousemove", () => {
+    square.style.backgroundColor = "black";
   });
+}
+
+function pageOpen() {
+  return Number(prompt("Enter square quantity (1-100)"));
 }
