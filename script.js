afd88d7410ce228cf5a1b7d516b56df504986795
EugeneDev <kovalonokjevgenij@gmail.com>
2026-01-13 07:38:03 +0200

    add rainbow and opacity functionality

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 const okno = document.querySelector("#container");
 const resetButton = document.querySelector("#reset");
-
+const rainbowButton = document.querySelector("#rainbow");
 const contSize = 500;
-
-createGrid(getSize());
+const size = getSize();
+createGrid(size);
 
 resetButton.addEventListener("click", () => {
   createGrid(getSize());
+});
+rainbowButton.addEventListener("click", () => {
+  okno.innerHTML = "";
+  okno.style.width = contSize + "px";
+
+  let squareSize = contSize / size;
+  for (let i = 0; i < size * size; i++) {
+    const square = document.createElement("div");
+    square.classList.add("grid_square");
+
+    square.style.width = squareSize + "px";
+    square.style.height = squareSize + "px";
+    let timesOn = 0;
+    square.addEventListener("mousemove", () => {
+      let r = Math.floor(Math.random() * 256);
+      let g = Math.floor(Math.random() * 256);
+      let b = Math.floor(Math.random() * 256);
+      square.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    });
+
+    okno.appendChild(square);
+  }
 });
 
 function getSize() {
@@ -20,16 +42,21 @@ function createGrid(size) {
   okno.style.width = contSize + "px";
 
   let squareSize = contSize / size;
-
   for (let i = 0; i < size * size; i++) {
     const square = document.createElement("div");
     square.classList.add("grid_square");
 
     square.style.width = squareSize + "px";
     square.style.height = squareSize + "px";
-
+    let timesOn = 0;
     square.addEventListener("mousemove", () => {
+      timesOn++;
+      let opacity = 0.1 * timesOn;
+      if (timesOn >= 10) {
+        opacity = 1;
+      }
       square.style.backgroundColor = "black";
+      square.style.opacity = opacity;
     });
 
     okno.appendChild(square);
